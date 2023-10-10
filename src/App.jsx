416b641241dd9016a306f7e2f80f18568e4105f9
Jordan Mieger <144772550+JordanMieger@users.noreply.github.com>
2023-10-10 14:32:31 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import PokemonCard from "./components/PokemonCards"
 
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+    
+  },
+];
+
 
 function App() {
+  
   return (
     <div>
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]}/>
     </div>
   );
 }
