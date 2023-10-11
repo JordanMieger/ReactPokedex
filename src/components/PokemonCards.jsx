06d209c122f224d094214ce8873f8c05c56b68e9
JordanMieger <miegerjordan2000@gmail.com>
2023-10-11 11:21:31 +0200

    Reacts Basics 05 Props2

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from "prop-types";
 
 function PokemonCard(props) {
     console.log(props)
@@ -9,6 +9,15 @@ function PokemonCard(props) {
         <figcaption>{pokemon.name}</figcaption></figure>
         )
     }
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string
+    }).isRequired,
+}
+
+
 
     export default PokemonCard;
     
